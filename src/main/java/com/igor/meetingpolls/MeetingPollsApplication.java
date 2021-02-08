1bb957a7adfe11cf3ea7a2e0f9e04b14f3ca4d18
Igor Moraes <igor.moraes@netcracker.com>
2021-02-08 12:07:20 -0300

    First commit

---
+++ src/main/java/com/igor/meetingpolls/MeetingPollsApplication.java
@@ -0,0 +1,13 @@
+package com.igor.meetingpolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPollsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingPollsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igor/meetingpolls/MeetingPollsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.meetingpolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPollsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
